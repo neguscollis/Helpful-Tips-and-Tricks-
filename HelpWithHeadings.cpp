acbfa950059e6aebcbc508d74371791c79152a81
neguscollis <35749270+neguscollis@users.noreply.github.com>
2021-09-05 15:24:56 -0700

    Add files via upload

---
+++ HelpWithHeadings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>// So you could start using cout and cin in your code
+using namespace std; // So you won't have to keep typing std:: before everthing
+
+
+
+
+
+int main () {// This is where all of your main code with starts
+
+cout << "Hello World!!" << endl;// Start of your first coding program
+
+
+
+
+
+  return 0;
+}// This where the code will end
