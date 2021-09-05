fedab6a7297e3e6d8e2f9bfd2e9db8348b496a0a
neguscollis <35749270+neguscollis@users.noreply.github.com>
2021-09-05 15:56:02 -0700

    Update HelpWithHeadings.cpp

--- HelpWithHeadings.cpp
+++ HelpWithHeadings.cpp
@@ -18,12 +18,13 @@ using namespace std; // So you won't have to keep typing std:: before everthing
 int main () {// This is where all of your main code with starts
 
 cout << "Hello World!!" << endl;// Start of your first coding program
-
+cout << "Hello World!!\n"; // Instead of using endl
 
 
 
 
   return 0;
 }// This where the code will end
+
 
 
