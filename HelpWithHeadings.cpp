88db475382a07f48757d16aacb64e6a75c882369
neguscollis <35749270+neguscollis@users.noreply.github.com>
2021-09-05 15:34:08 -0700

    Update HelpWithHeadings.cpp

--- HelpWithHeadings.cpp
+++ HelpWithHeadings.cpp
@@ -3,6 +3,13 @@ using namespace std; // So you won't have to keep typing std:: before everthing
 
 
 
+//How to compile your program on a Mac in Terminal:
+// g++ -o (What you want to name your program) (Yourprogram.cpp)
+// You'll need to type ./(name of file) to run Your program
+
+
+
+
 
 
 int main () {// This is where all of your main code with starts
@@ -15,3 +22,4 @@ cout << "Hello World!!" << endl;// Start of your first coding program
 
   return 0;
 }// This where the code will end
+
